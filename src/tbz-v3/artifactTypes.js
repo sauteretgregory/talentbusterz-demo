ea2afc2d0793f89c838871344f688d5f975d9fa4
Gregory Sauteret <estangar@macbook-air-de-gregory.home>
2026-08-12 17:46:36 +0200

    Align job artifact type with canonical V3 state

--- src/tbz-v3/artifactTypes.js
+++ src/tbz-v3/artifactTypes.js
@@ -1,6 +1,6 @@
 export const TBZ_ARTIFACT_TYPES = Object.freeze({
   CANDIDATE: 'canonical_candidate_data_state',
-  JOB: 'canonical_job_state',
+  JOB: 'canonical_job_data_state',
   MATCH: 'canonical_match_state',
   PROBE_PLAN: 'canonical_probe_plan',
   PROBE_DIALOG: 'canonical_probe_dialog_state'
