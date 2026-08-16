7939e0d4615c9fe43162cded91bbd27376faf529
GREGORY SAUTERET <sauteret.gregory@gmail.com>
2026-08-16 16:21:16 +0200

    feat(ui): make persistent memory flow the default entry

---
+++ src/tbz-v3-entry.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import V3Preview from './tbz-v3/V3Preview.jsx'
+import './styles.css'
+
+createRoot(document.getElementById('root')).render(<V3Preview />)
